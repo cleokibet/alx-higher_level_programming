f49fd20ac125192d2b6d1e1a96e3d57febfdfb7c
cleokibet <cleokibet@gmail.com>
2023-05-11 22:15:16 +0300

    updated

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from variable_load_5 import a
 
 
-def find_var():
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a
+
     print(a)
-    if __name__ == "__main__":
-        find_var()
