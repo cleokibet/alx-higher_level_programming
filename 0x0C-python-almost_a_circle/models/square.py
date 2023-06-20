94d690d6fcd273789376df6a6e59c83765878f22
cleokibet <cleokibet@gmail.com>
2023-06-20 05:02:51 +0300

    0x0C-python-almost_a_circle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+"""Writing the class Square that inherits from Rectangle"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    defines class Square; inherits from class Rectangle
+    Inherited Attributes:
+        id
+        __weight        __height
+        __x             __y
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter size - sets width and height as size"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Prints [Square] (<id>)"""
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
+            self.__class__.__name__, self.id, self.x, self.y,
+            self.size)
+
+    def update(self, *args, **kwargs):
+        """
+        If args: set attributes in this order: id, width, height
+        """
+        if args:
+            for k, v in enumerate(args):
+                if k == 0:
+                    self.id = v
+                elif k == 1:
+                    self.size = v
+                elif k == 2:
+                    self.x = v
+                else:
+                    self.y = v
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """Return dictionary representation"""
+        d = {}
+        d["id"] = self.id
+        d["size"] = self.size
+        d["x"] = self.x
+        d["y"] = self.y
+        return d
