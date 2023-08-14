94055904ff1594a8354d03531b1ba92181c43db1
cleokibet <cleokibet@gmail.com>
2023-08-14 23:48:03 +0300

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,3 @@
-t { argv } = require('process');
+#!/usr/bin/node
+const { argv } = require('process');
 if (argv.length === 2) { console.log('No argument'); } else if (argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
